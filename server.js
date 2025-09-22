db10b67303c99f73b444e4080edd7c850fdbd17d
juju1001 <54131638+juju1001@users.noreply.github.com>
2025-09-22 18:01:25 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,12 +7,7 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "https://title-generator-backend-production.up.railway.app"],
-    credentials: true,
-  })
-);
+app.use(cors())
 
 app.use(express.json());
 
